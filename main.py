5643901d6fa763d832d38ab365bcd5a8b1f12964
IxAntho <isabar735@icloud.com>
2024-02-04 00:30:32 -0500

    complete project

--- main.py
+++ main.py
@@ -1,14 +1,54 @@
+from tkinter import *
+from tkinter import messagebox as msb
+from random import randint, choice, shuffle
+import pyperclip
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+           'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+           'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+
+def generate_password():
+    password_letter = [choice(letters) for _ in range(randint(8, 10))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+
+    password_list = password_letter + password_symbols + password_numbers
+
+    shuffle(password_list)
+
+    new_password = ''.join(password_list)
+
+    password_entry.insert(END, new_password)
+
+    pyperclip.copy(new_password)
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
-    pass
+    website = website_entry.get()
+    email = user_entry.get()
+    password = password_entry.get()
+
+    if len(website) == 0 or len(email) == 0 or len(password) == 0:
+        msb.showerror(title="Oops", message="Please don't leave any fields empty!")
+        return
+    else:
+        is_ok = msb.askyesnocancel(title=website,
+                                   message=f"These are the details entered: \nEmail: {email} \nPassword: {password} \nIs it ok to save?")
+
+        if is_ok:
+            with open("data.txt", "a") as data:
+                data.write(f"{website} | {email} | {password}\n")
+
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
+
+
 # ---------------------------- UI SETUP ------------------------------- #
-# NORMAL_FONT = ("Arial", 15, "normal")
-# PASSWORD_BTTN_FONT = ("Arial", 12, "normal")
-
-from tkinter import *
-
 window = Tk()
 window.title("Password Manager")
 window.config(padx=20, pady=20)
@@ -38,13 +78,13 @@ user_entry.insert(END,
 # Row 3
 password_label = Label(text="Password:")
 password_label.grid(column=0, row=3)
-password_entry = Entry(width=21, bg="white")
+password_entry = Entry(width=21, show="*")
 password_entry.grid(column=1, row=3)
-password_button = Button(text="Generate Password", width=12)
+password_button = Button(text="Generate Password", width=12, command=generate_password)
 password_button.grid(column=2, row=3)
 
 # Row 4
-add_button = Button(text="Add", width=35)
+add_button = Button(text="Add", width=35, command=save)
 add_button.grid(column=1, row=4, columnspan=2)
 
 window.mainloop()
